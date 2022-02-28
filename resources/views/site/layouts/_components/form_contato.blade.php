c89bb9bbcfd310936e0c179a63b2259f725bbfd9
Rafael Gusmao <rafaelgusmao@MacBook-Air-de-Rafael.local>
2022-02-28 17:54:56 -0300

    Configuracao inicial do MySql

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,20 +2,20 @@
 {{ $classe }}
 <form action={{ route('site.contato') }} method="POST">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class= {{ $classe }}>
+    <input name="nome" type="text" placeholder="Nome" class= "{{ $classe }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class={{ $classe }}>
+    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class={{ $classe }}>
+    <input name="email" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
-    <select name="contato" class={{ $classe }}>
+    <select name="contato" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
         <option value="1">Dúvida</option>
         <option value="2">Elogio</option>
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class={{ $classe }}>Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
     <br>
-    <button type="submit" class={{ $classe }}>ENVIAR</button>
+    <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
